12cff02fbf69c7ad500e5b334bb8de99b72c35a3
Plostins <plostinse.kekava@gmail.com>
2022-01-20 15:04:01 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Jana Bērziņa','Rūdolfs Liepa','Jānis Ozols','Rita Pipare','Fredis Bumba']
+const balvas=['Auto','Mobilais','PC','Pica','Kebabs','Miljons']
+const naudasKopa=100000;//kopējā summa
+let uzvaretajuSkaits=5;
+let rand= Math.random()*5;
+rand= Math.floor(rand);//noapaļo uz leju
+console.log(vardi[rand]);//izvada konsolē
