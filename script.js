d70ab50f6b32f365d10772030836f738f7037b3d
Plostins <plostinse.kekava@gmail.com>
2022-01-27 15:12:07 +0200

    go

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const vardi=['Jana Bērziņa','Rūdolfs Liepa','Jānis Ozols','Rita Pipare','Fredis Bumba']
-const balvas=['Auto','Mobilais','PC','Pica','Kebabs','Miljons']
-const naudasKopa=100000;//kopējā summa
-let uzvaretajuSkaits=5;
-let rand= Math.random()*5;
-rand= Math.floor(rand);//noapaļo uz leju
-console.log(vardi[rand]);//izvada konsolē
+const vardi = ['Jana Bērziņa', 'Rūdolfs Liepa', 'Jānis Ozols', 'Rita Pipare', 'Fredis Bumba']
+const balvas = ['Auto', 'Mobilais', 'PC', 'Pica', 'Kebabs', 'Miljons']
+const naudasKopa = 100000;//kopējā summa
+let uzvaretajuSkaits = 5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = '';
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);//noapaļo uz leju
+
+    let uzvaretajs = vardi[rand];//izvada konsolee
+    rindas.innerHTML += `
+
+    <tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>
+    </tr>`
+}
